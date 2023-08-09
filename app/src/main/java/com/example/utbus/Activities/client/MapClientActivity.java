eece11826e692cf0893f7dd9c5dcd7dfffdded69
JoshuaSantiago19 <jonathanjoshua18@gmail.com>
2023-08-08 21:47:50 -0600

    toolbar mapa

--- app/src/main/java/com/example/utbus/Activities/client/MapClientActivity.java
+++ app/src/main/java/com/example/utbus/Activities/client/MapClientActivity.java
@@ -1,19 +1,26 @@
 package com.example.utbus.Activities.client;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.utbus.Activities.MainActivity;
 import com.example.utbus.R;
 import com.example.utbus.providers.AuthProvider;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
-public class MapClientActivity extends AppCompatActivity {
+public class MapClientActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    Button mButtonLogout;
+    private GoogleMap mMap;
+    private SupportMapFragment mMapFragment;
     AuthProvider mAuthprovider;
 
 
@@ -22,17 +29,36 @@ public class MapClientActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_client);
 
-        mButtonLogout = findViewById(R.id.btnLogout);
         mAuthprovider = new AuthProvider();
+        mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mMapFragment.getMapAsync(this);
 
-        mButtonLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mAuthprovider.logout();
-                Intent intent = new Intent(MapClientActivity.this, MainActivity.class);
-                   startActivity(intent);
-                    finish();
-            }
-        });
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.driver_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_logout) {
+            logout();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    void logout (){
+        mAuthprovider.logout();
+        Intent intent = new Intent(MapClientActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/utbus/Activities/driver/MapDriverActivity.java
+++ app/src/main/java/com/example/utbus/Activities/driver/MapDriverActivity.java
@@ -3,9 +3,16 @@ package com.example.utbus.Activities.driver;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.example.utbus.Activities.MainActivity;
+import com.example.utbus.Activities.client.MapClientActivity;
 import com.example.utbus.R;
+import com.example.utbus.includes.MyToolbar;
+import com.example.utbus.providers.AuthProvider;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -14,12 +21,15 @@ public class MapDriverActivity extends AppCompatActivity implements OnMapReadyCa
 
     private GoogleMap mMap;
     private SupportMapFragment mMapFragment;
+    private AuthProvider mAuthprovider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_driver);
+        MyToolbar.show(this, "Conductor" ,false);
 
+        mAuthprovider = new AuthProvider();
         mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mMapFragment.getMapAsync(this);
     }
@@ -29,4 +39,26 @@ public class MapDriverActivity extends AppCompatActivity implements OnMapReadyCa
         mMap = googleMap;
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.driver_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_logout) {
+            logout();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    void logout (){
+        mAuthprovider.logout();
+        Intent intent = new Intent(MapDriverActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
